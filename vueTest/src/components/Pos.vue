f459f8fc4d0637a1d168a2905f7b5d5240475d19
梁娟 <liangjuan@mail.taiji.com.cn>
2019-06-03 17:26:12 +0800

    课时2

---
+++ vueTest/src/components/Pos.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="pos">
+    Hello  AwesomePos Demo
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Pos"
+    }
+</script>
+
+<style scoped>
+
+</style>
